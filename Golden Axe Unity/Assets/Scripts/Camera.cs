548928e42646363517ad1faab03c1fa89149b0e5
Ivan Olovyannikov <iolovyannikov@yandex.ru>
2020-08-06 21:00:13 +0300

    Написал скрипт камеры для слежения за игроком

---
+++ Golden Axe Unity/Assets/Scripts/Camera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform target;
+    int distrance = -10;
+    float lift = 1.5f;
+
+    void Update() {
+        transform.position = new Vector3(0, lift, distrance) + target.position;
+        transform.LookAt(target);
+    }
+}
