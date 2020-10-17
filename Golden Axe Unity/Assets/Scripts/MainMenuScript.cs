015868c7c8f233bd8d4e9286c8df9aa2251a6fad
Ivan Olovyannikov <iolovyannikov@yandex.ru>
2020-10-17 18:03:14 +0300

    New Scenes - Load/Save screen on start menu, new objects on a map

---
+++ Golden Axe Unity/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void startFunction() {
+        SceneManager.LoadScene("Level_1");
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
